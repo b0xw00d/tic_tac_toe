bec950ca63c96af0b16cebc83da577fd2d6fba0c
Chase Anthony Rogers <chase@bellwethermade.com>
2016-06-06 22:30:35 -0500

    feat(tic_tac_toe): check horizontal and vertical wins in game_over
    - check for horizontal and vertical wins in game_over function

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -15,10 +15,32 @@ defmodule TicTacToe do
     {:ok, new_board}
   end
 
+  def game_over?(board) do
+    horizontal_win(board) || vertical_win(board)
+  end
+
+  defp horizontal_win(board) do
+    Enum.chunk(board.cells, 3) |> Enum.any? fn(x) ->
+      Enum.all?(x, &(&1 == :x || &1 == :o))
+    end
+  end
+
+  defp vertical_win(board) do
+    transpose(board) |> Enum.any? fn(x) ->
+      Enum.all?(x, &(&1 == :x || &1 == :o))
+    end
+  end
+
+  defp transpose(board) do
+    Enum.chunk(board.cells, 3)
+    |> List.zip
+    |> Enum.map fn(x) -> Tuple.to_list(x) end
+  end
+
   defp marker(current_turn) when rem(current_turn, 2) == 0 do
     @markers |> List.first
   end
 
-  defp marker(current_turn), do: @markers |> List.last
+  defp marker(_), do: @markers |> List.last
 
 end
--- test/tic_tac_toe_test.exs
+++ test/tic_tac_toe_test.exs
@@ -6,33 +6,53 @@ defmodule TicTacToeTest do
     facts "%Board" do
       setup do
         board = %TicTacToe.Board{}
-        {:ok, board: board}
+        {:ok, default_board: board}
       end
 
-      fact "starts with an empty board", %{board: board} do
-        board.cells |> equals [nil, nil, nil, nil, nil, nil, nil, nil, nil]
+      fact "starts with an empty board", %{default_board: default_board} do
+        default_board.cells |> equals [nil, nil, nil, nil, nil, nil, nil, nil, nil]
       end
 
-      fact "defaults to 0", %{board: board}  do
-        board.current_turn |> equals 0
-        board.current_turn |> !equals 1
+      fact "defaults to 0", %{default_board: default_board}  do
+        default_board.current_turn |> equals 0
+        default_board.current_turn |> !equals 1
       end
     end
 
     facts ".take_turn" do
       setup do
-        board = %TicTacToe.Board{}
-        {:ok, turn} = TicTacToe.take_turn(board, 0)
-        {:ok, turn: turn}
+        default_board = %TicTacToe.Board{}
+        {:ok, board} = TicTacToe.take_turn(default_board, 0)
+        {:ok, board: board}
       end
 
-      fact "places a marker", %{turn: turn} do
-        turn.cells |> equals [:x, nil, nil, nil, nil, nil, nil, nil, nil]
+      fact "places a marker", %{board: board} do
+        board.cells |> equals [:x, nil, nil, nil, nil, nil, nil, nil, nil]
       end
 
-      fact "allows players to take turns", %{turn: turn} do
-        {:ok, turn_2} = TicTacToe.take_turn(turn, 1)
+      fact "allows players to take turns", %{board: board} do
+        {:ok, turn_2} = TicTacToe.take_turn(board, 1)
         turn_2.cells |> equals [:x, :o, nil, nil, nil, nil, nil, nil, nil]
+      end
+    end
+
+    facts ".game_over?" do
+      fact "it returns false for boards without a win" do
+        TicTacToe.game_over?(%TicTacToe.Board{}) |> equals false
+      end
+
+      fact "it finds horizontal wins" do
+        horizontal = %TicTacToe.Board{
+          cells: [:x, :x, :x, nil, nil, nil, nil, nil, nil]
+        }
+        TicTacToe.game_over?(horizontal) |> equals true
+      end
+
+      fact "it finds vertical wins" do
+        vertical = %TicTacToe.Board{
+          cells: [:x, nil, nil, :x, nil, nil, :x, nil, nil]
+        }
+        TicTacToe.game_over?(vertical) |> equals true
       end
     end
 
